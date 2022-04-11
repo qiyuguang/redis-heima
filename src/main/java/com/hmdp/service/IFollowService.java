2110e2df495c7eeafbc61e97ffba3c84a928e6da
guang <qiyuguang007@vip.qq.com>
2022-04-11 17:32:03 +0800

    关注用户

--- src/main/java/com/hmdp/service/IFollowService.java
+++ src/main/java/com/hmdp/service/IFollowService.java
@@ -1,7 +1,10 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Follow;
 import com.baomidou.mybatisplus.extension.service.IService;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -13,4 +16,7 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IFollowService extends IService<Follow> {
 
+    Result follow(Long followUserId, Boolean isFollow);
+
+    Result isFollow(Long followUserId);
 }
